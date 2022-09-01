227c528e718b851b029245b8d8685a0c6eb34ef1
gineude <gineude@gmail.com>
2022-09-01 08:57:31 -0300

    Trabalhando com delete.

--- src/app/courses/course-info-component.ts
+++ src/app/courses/course-info-component.ts
@@ -13,11 +13,26 @@ export class CourseInfoComponent implements OnInit {
     constructor(private activateRoute: ActivatedRoute, private courseService: CourseService) {};
 
     ngOnInit(): void {
-        this.course = this.courseService.retrieveById(Number(this.activateRoute.snapshot.paramMap.get('id')));
+        const idCourse = Number(this.activateRoute.snapshot.paramMap.get('id'));
+        this.courseService.retrieveById(idCourse).subscribe({
+            next: course => {
+                this.course = course;
+            },
+            error: err => {
+                console.log('Error', err);
+            }
+        });
     }
 
     save() : void {
-        this.courseService.save(this.course);
+        this.courseService.saveOrUpdate(this.course).subscribe({
+            next: course => {
+                this.course = course;
+            },
+            error: err => {
+                console.log('Error', err);
+            }
+        });
     }
 
 }
